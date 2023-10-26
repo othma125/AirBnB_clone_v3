a2bb18fcf664d0b66ee55bfd9289ce9a3f8e7a9c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-26 17:17:16 +0100

    api

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Contains the class DBStorage
+"""
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """teardown"""
+    storage.close()
+
+
+
+if __name__ == '__main__':
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
