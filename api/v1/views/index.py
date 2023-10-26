86688a1f0b58a7799260a2f107def20a6a271672
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-26 17:35:03 +0100

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,9 @@
 import app_views from api.v1.views
 """
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
