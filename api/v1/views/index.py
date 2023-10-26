ce163c8f3853e49e4da7b0ea3bce696d023030c0
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-26 17:39:05 +0100

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,22 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 def status():
     """return status"""
     return {"status": "OK"}
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """return stats"""
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    return {"amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)}
