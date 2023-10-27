6031ffd6733fd3bdaf5625348c23e3430b210969
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-27 18:21:39 +0100

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,16 @@ Contains the class DBStorage
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(self):
     """teardown"""
     storage.close()
-
 
 @app.errorhandler(404)
 def not_found(error):
