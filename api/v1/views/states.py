0c6cd0f0fe93fef5e7809ecf31a892a7d3b3f491
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-27 17:26:42 +0100

    api/v1/views/cities.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -60,7 +60,8 @@ def update_state(state_id):
     if not data:
         abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
+        if key in ['id', 'created_at', 'updated_at']:
+            continue
+        setattr(state, key, value)
     state.save()
     return make_response(jsonify(state.to_dict()), 200)
