e5e0e5c01fcb4b6627376579ec91d64056af1f14
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-27 18:22:31 +0100

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown(self):
     """teardown"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
