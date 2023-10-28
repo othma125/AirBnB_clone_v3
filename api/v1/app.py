2429cb8209399ee4deaca0d78d2151b276978e1e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-10-28 19:31:31 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 from flask_cors import CORS
-from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
