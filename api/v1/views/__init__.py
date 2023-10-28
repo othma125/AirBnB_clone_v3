1025fbc88874450d36694ec710b20d1821eeb1f7
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-28 17:46:22 +0100

    api/v1/views/users.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """
 initialize the views package
 """
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,3 +11,7 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,7 @@
 """
 import app_views from api.v1.views
 """
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
