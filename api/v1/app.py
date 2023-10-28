a0c160d347c211bf54844192dab95f4214321280
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-28 17:59:55 +0100

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -32,7 +33,7 @@ swagger = Swagger(app)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown_session(exception):
     """teardown"""
     storage.close()
 
@@ -40,12 +41,14 @@ def teardown(self):
 @app.errorhandler(404)
 def not_found(error):
     """handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
     from os import getenv
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
 
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
+    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
+    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
