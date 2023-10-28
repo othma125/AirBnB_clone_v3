6e64018a771cc1d40a1c7cdc6c27de3a4d7413bd
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-28 15:19:30 +0100

    models/base_model.py

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,30 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        super().__init__(*args, **kwargs)
+        if kwargs:
+            if "password" in kwargs:
+                encoded = kwargs["password"].encode()
+                kwargs.updates({"password": md5(encoded).hexdigest()})
+            super().__init__(*args, **kwargs)
+
+    def to_dict(self, save_password=False):
+        """Return dictionary representation of User instance"""
+        new_dict = super().to_dict()
+        if not save_password:
+            new_dict.pop("password", None)
+        return new_dict
+
+    @property.getter
+    def password(self):
+        """Getter for password"""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """Setter for password"""
+        self.__password = md5(value.encode()).hexdigest()
+
+    def save(self):
+        """Save User instance"""
+        self.password = self.__password
+        super().save()
