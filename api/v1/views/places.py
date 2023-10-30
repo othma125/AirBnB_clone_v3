6c2c771226e73dc811b3694996d6146ab7b8d514
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:40:47 +0100

    users

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -34,7 +34,7 @@ def get_place(place_id):
 @app_views.route('/places/<place_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
-    """Deletes a Place object"""
+    """Deletes a Place object by id"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
