ec685b0dd53057df80b5e31b4006ed3852c3606d
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:30:24 +0100

    place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -66,7 +66,8 @@ def create_place(city_id):
     return jsonify(place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
     place = storage.get(Place, place_id)
