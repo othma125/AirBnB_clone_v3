20bf913f2c8d5a0fdb3547279e262a70b89c6d7d
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:27:14 +0100

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -50,7 +50,8 @@ def create_state():
     return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """update a state"""
     state = storage.get(State, state_id)
