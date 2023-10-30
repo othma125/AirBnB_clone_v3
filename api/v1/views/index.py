d0bfeec9705f31ae70f199450ca2bfedddaca514
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-30 16:03:43 +0100

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,6 @@
 App views for AirBnB_clone_v3
 """
 
-from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,21 +10,17 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def status():
     """ returns status """
-    status = {"status": "OK"}
-    return jsonify(status)
+    return {"status": "OK"}
 
 
 @app_views.route('/stats')
 def count():
     """ returns number of each objects by type """
-    total = {}
     classes = {"Amenity": "amenities",
                "City": "cities",
                "Place": "places",
                "Review": "reviews",
                "State": "states",
                "User": "users"}
-    for cls in classes:
-        count = storage.count(cls)
-        total[classes.get(cls)] = count
-    return jsonify(total)
+    return {classes.get(cls): storage.count(cls)
+            for cls in classes}
