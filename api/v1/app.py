096a460c8046659165dcf0c43856474befe4b8d4
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:36:21 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class DBStorage
+Contains the class DBStorage that handles all database storage
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify
