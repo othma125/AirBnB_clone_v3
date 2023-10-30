0390dc481b97ec937afaa4bf2d0f991e09108cb4
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:29:28 +0100

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Create a new view for State objects that handles
-all default RESTFul API actions
+Develop a new view for State objects that manages
+all standard RESTful API operations.
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views
