44a01f1a9027bf5d3364645e81a93cf9b55c4ca4
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:37:37 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,6 +26,7 @@ def not_found(error):
 
 
 if __name__ == '__main__':
+    """main method"""
     from os import getenv
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
