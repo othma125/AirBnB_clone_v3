d77b556171d51b1b759f17126392475cfe7196ab
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:53:39 +0100

    from models import storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
-    """ returns status """
+    """ returns status OK """
     return jsonify({"status": "OK"})
 
 
