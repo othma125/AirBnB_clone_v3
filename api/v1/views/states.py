d7d54b1d5462721b7dc5f7bfd1203514af0f6d39
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:40:19 +0100

    users

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -42,7 +42,7 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'],
                  strict_slashes=False)
 def create_state():
-    """create a state object"""
+    """create a state object from a HTTP request"""
     if not request.is_json:
         abort(400, 'Not a JSON')
     data = request.get_json()
