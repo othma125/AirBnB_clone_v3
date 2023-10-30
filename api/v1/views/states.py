1c38aa73e17b612a602f54d43b0767e427adcbb8
bensbahou <younesbensbahou@gmail.com>
2023-10-30 21:39:53 +0100

    users

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -56,7 +56,7 @@ def create_state():
 @app_views.route('/states/<state_id>',
                  methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """update a state"""
+    """update a state object"""
     state = storage.get(State, state_id)
     if not state:
         abort(404)
