d4775bcdda14b5e487e5c2b57b05a74a48771e13
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-11-02 14:06:49 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,72 +1,86 @@
 #!/usr/bin/python3
-"""
-Develop a new view for State objects that manages
-all standard RESTful API operations.
-"""
-from flask import jsonify, abort, request
-from api.v1.views import app_views
-from models import storage
+""" objects that handle all default RestFul API actions for States """
 from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/states', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """get all states"""
-    return jsonify([state.to_dict()
-                    for state in storage.all(State).values()])
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>',
-                 methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
-    """get a state by id"""
+    """ Retrieves a specific State """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
+
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_state(state_id):
-    """delete a state by id"""
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    state.delete()
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/states', methods=['POST'],
+@app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def create_state():
-    """create a state object from a HTTP request"""
-    if not request.is_json:
-        abort(400, 'Not a JSON')
-    data = request.get_json()
-    if 'name' not in data:
-        abort(400, 'Missing name')
-    state = State(**data)
-    state.save()
-    return jsonify(state.to_dict()), 201
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
 
-
-@app_views.route('/states/<state_id>',
-                 methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """update a state object by id"""
     state = storage.get(State, state_id)
+
     if not state:
         abort(404)
-    if not request.is_json:
-        abort(400, 'Not a JSON')
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """
+    Creates a State
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
     data = request.get_json()
-    keys = 'id', 'created_at', 'updated_at'
+    instance = State(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """
+    Updates a State
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
     for key, value in data.items():
-        if key in keys:
-            continue
-        setattr(state, key, value)
-    state.save()
-    return jsonify(state.to_dict()), 200
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
